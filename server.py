d977093217508f2f7c820e9bc246eea8e7eb3b41
Jesslyn Landgren <jesslyn.landgren@gmail.com>
2016-10-29 00:35:32 -0400

    merging

--- server.py
+++ server.py
@@ -19,7 +19,6 @@ db = pg.DB(
 
 @app.route('/')
 def render_homepage():
-    pages = db.query("select p.pagename, max(c.timestamp) from pages as p, content as c where p.id=c.pageid group by p.pagename").namedresult()
     loggedin = False
     try:
         session['username']
@@ -28,8 +27,8 @@ def render_homepage():
         loggedin = False
 
     return render_template(
-        'homepage.html',
-        title="Jesslyn's Wiki",
+        'login.html',
+        title="SoccerStreets",
         pages = pages,
         loggedin = loggedin
     )
@@ -42,7 +41,7 @@ def submit_login():
     if len(results) > 0:
         user = results[0]
         if user.password == password:
-            session['username'] = user.username
+            session['id'] = user.id
             flash("Successfully Logged In")
             return redirect('/')
         else:
@@ -55,3 +54,92 @@ def logout():
     del session['username']
     flash("Successfully Logged Out")
     return redirect('/')
+
+@app.route('/checkin', methods=['POST'])
+def checkin():
+    kid_id = request.form.get('kid_id');
+    timestamp = time.time();
+    origin =request.form.get('origin');
+    destination = request.form.get('destination');
+    query = db.insert('chekins',{
+    'timestamp' : timestamp,
+    'kid_id' : kid_id,
+    'chaparone_id' : session[id],
+    'origin_id' : origin,
+    'dest_id' : destination
+    })
+        return render_template(
+        'checkin_submit.html',
+        kid_qr = kid_qr;
+        timestamp = timestamp;
+        origin = origin;
+        destination = destination;
+        )
+
+@app.route('/checkin5', methods=['POST'])
+def checkin5():
+        query = db.query("")
+
+
+@app.route('/checkout', methods=['POST'])
+def checkout():
+
+
+@app.route('/parent')
+def render_parent():
+    # Query to get all the parent's kids
+    kids_list = db.query('')
+    return render_template(
+        'parent.html',
+        kids_list = kids_list
+    )
+
+@app.route('/chaperone')
+def render_chaperone():
+    return render_template(
+        'chaperone.html'
+    )
+
+@app.route('/kid')
+def render_chaperone():
+    kids_list = # Query to get all the kids
+    return render_template(
+        'kid.html'
+    )
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file_name_size = 15
+    user_id = request.form.get("id")
+    print "user id %s\n\n\n" % user_id
+    project_id = request.form.get("project_id")
+    print "project id %s\n\n\n" % project_id
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        file_extension = filename.rsplit('.', 1)[1]
+        # Create new random name for the file using letters and digits
+        filename = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(file_name_size)]) + "." + file_extension
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basically show on the browser the uploaded file
+        if user_id:
+            db.update (
+                "users", {
+                    "id": user_id,
+                    "avatar": filename
+                }
+            )
+        elif project_id:
+            db.insert (
+                "image",
+                image = filename,
+                project_id = project_id
+            )
+        return redirect('/')
