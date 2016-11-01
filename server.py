7a9a791b8d69fc16fecc50fa42a4ebfb2c1d3406
Keyur Patel <keyur@Keyurs-MacBook-Pro.local>
2016-11-01 17:22:27 -0400

    error on app is not defined

--- server.py
+++ server.py
@@ -7,10 +7,11 @@ from dotenv import load_dotenv, find_dotenv
 import os
 import requests
 
-QRcode(app)
+
 # load_dotenv(find_dotenv())
 # tmp_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask('SoccerStreets')  # , template_folder=tmp_dir
+QRcode(app)
 app.secret_key = "ksajoivnvaldksdjfj"
 db = pg.DB(
     dbname=os.environ.get('PG_DBNAME'),
